b02ad87bf388b28bc454f1f23939b56d131092a3
riyamhatre <76131039+riyamhatre@users.noreply.github.com>
2023-05-21 22:17:54 -0700

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -16,7 +16,7 @@ def app():
 	with header:
 	    st.write('<p style="font-size:32px;"><b>Zip Code Recommendation Based on Given Factors</b></p>', unsafe_allow_html=True)
 	    st.write('<p style="font-size:20px;">This part of the website consists of the model. Here, you will input your criteria. After answering the questions on the page and hitting the button at the bottom, you can head to the Zip Code Statistics tab at the left to see some relevant data about your recommended zip code. </p>', unsafe_allow_html=True)
-	location = pd.read_csv('/Users/riyamhatre/Desktop/HDSI/location.csv')
+	location = pd.read_csv('location.csv')
 	z = st.text_input("Enter a Zip Code!")
 	if len(z) >1:
 		st.map(location[location['ZIP'] == int(z)])
